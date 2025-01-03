bf70fb17b0a7cd135e9fe7cfab64c92205a9e92e
Yaseen <121yaseen@gmail.com>
2025-01-04 01:51:47 +0530

    make /inventory default landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,29 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import router from "next/router";
+import { useEffect } from "react";
+import Loader from "./components/shared/LoaderComponent";
+import { redirect } from "next/navigation";
+
 export default function Home() {
+  const { status } = useSession();
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/login"); // Redirect to login if not authenticated
+    } else if (status === "authenticated") {
+      router.push("/inventory"); // Redirect to dashboard if authenticated
+    }
+  }, [status]);
+
+  if (status === "loading") {
+    return <Loader isVisible={true} />;
+  }
+  if (status === "authenticated") {
+    redirect("/inventory");
+  }
+  if (status === "unauthenticated") {
+    redirect("/login");
+  }
   return null;
 }
