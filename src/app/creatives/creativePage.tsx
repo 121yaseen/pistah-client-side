0bdab71c885caeb4813e7aa6a7c1ab118769a9cb
Josen Jose <josen.jose@soti.net>
2025-01-04 00:23:26 +0530

    added new page

---
+++ src/app/creatives/creativePage.tsx
@@ -0,0 +1,7 @@
+export default function CreativePageComponent() {
+  return (
+    <div className="min-h-screen w-full bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-100">
+      {/* <Loader isVisible={isLoading} /> */}
+    </div>
+  );
+}
---
+++ src/app/creatives/page.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/shared/Header"
+import CreativePageComponent from "./creativePage";
+
+export default function DashboardPage() {
+  return (
+    <>
+      <Header />
+      <main>
+        <CreativePageComponent />
+      </main>
+    </>
+  );
+}
