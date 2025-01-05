cdae0ea7498f020869fc5da964136ebf80766016
Josen Jose <josen.jose@soti.net>
2025-01-05 16:43:11 +0530

    reg fix 3

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -103,6 +103,7 @@ const Register = () => {
                 width={350}
                 height={350}
                 className="object-contain"
+                priority
               />
             </div>
           </div>
